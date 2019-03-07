84d8949c5fa2d37a12fc85e4509d280bebb08f96
This PC <abhishekchahar260@gmail.com>
2019-03-07 11:45:09 +0530

    init commit new login and password

--- app/src/main/java/aka/ansone/MainActivity.java
+++ app/src/main/java/aka/ansone/MainActivity.java
@@ -3,40 +3,59 @@ package aka.ansone;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText editText;
+    private EditText mail,pass;
+    private Button login;
+    String dMail,dPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mail=findViewById(R.id.editText);
+        pass=findViewById(R.id.editText2);
+        login=findViewById(R.id.button);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mail.getText().toString().equals(dMail)&&pass.getText().toString().equals(dPass)){
+                    Toast.makeText(MainActivity.this, "Correct, Login successful", Toast.LENGTH_SHORT).show();
+
+                }else if (mail.getText().toString().equals("")||pass.getText().toString().equals("")){
+                    Toast.makeText(MainActivity.this, "enter id and password", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(MainActivity.this, "id or password!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
-}
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        SharedPreferences sharedPreferences=getSharedPreferences("abhishek02.sharedprefdemo",MODE_PRIVATE);
+        SharedPreferences.Editor editor=sharedPreferences.edit();
+        editor.putString("mail","abhishek.singh_mca18@gla.ac.in");
+        editor.putString("pass","12345");
+        editor.apply();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences sharedPreferences=getSharedPreferences("abhishek02.sharedprefdemo",MODE_PRIVATE);
+        dMail=sharedPreferences.getString("mail","user");
+        dPass=sharedPreferences.getString("pass","password");
+
+    }
 
 
-@Override
-protected void onPause() {
-    super.onPause();
-    SharedPreferences sharedPreferences =
-            getSharedPreferences( "gla.abhihek.sharedprefdemo", MODE_PRIVATE );
-    SharedPreferences.Editor editor = sharedPreferences.edit();
-    editor.putString("name", editText.getText().toString().trim());
-    editor.putBoolean("bool", true);
-    editor.apply();
-
-}
-
-@Override
-protected void onResume() {
-    super.onResume();
-    SharedPreferences sharedPreferences =
-            getSharedPreferences( "gla.abhishek.sharedprefdemo", MODE_PRIVATE);
-    String s1 = sharedPreferences.getString( "name", false);
-    Boolean remember = sharedPreferences.getBoolean( "bool", false);
-    Toast.makeText( this, "bool" + remember, Toast.LENGTH_SHORT).show();
-    editText.setText(s1);
 }
